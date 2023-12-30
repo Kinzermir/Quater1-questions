f7205722e0db1bebc3c14141388c8558f66de395
Kinzermir <152514002+Kinzermir@users.noreply.github.com>
2023-12-30 23:34:39 +0500

    complete

---
+++ task44/index.js
@@ -0,0 +1,10 @@
+// function makeSandwitch (...item : string []): void {     // for not changing orignal syntax
+//     console.log("Creating s sandwitch with: ");
+//     item.forEach(Show => {
+//         console.log(`-${Show}`);
+//     });
+//     console.log("Sandwithc complete! \n");
+// }
+// makeSandwitch("panene","lettuce","cheese")
+// makeSandwitch("panene","lettuc")
+// makeSandwitch("panene")
